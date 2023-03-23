e4fafa3769ac97961c9176460da9ebf829993547
ht <22350747@qq.com>
2023-03-23 15:12:45 +0800

    改成 http 方式连接

--- internal/pkg/tsdb/tdengine.go
+++ internal/pkg/tsdb/tdengine.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/taosdata/driver-go/v3/taosSql"
+	_ "github.com/taosdata/driver-go/v3/taosRestful"
 )
 
 var Metrics = []string{"amounts", "txs", "txs_gas"}
@@ -14,7 +14,7 @@ var DSN string
 
 func Init(dsn string) error {
 	for i := range Metrics {
-		db, err := sql.Open("taosSql", dsn+"avata_"+Metrics[i])
+		db, err := sql.Open("taosRestful", dsn+"avata_"+Metrics[i])
 		if err != nil {
 			return fmt.Errorf("connect TDengine db %s, err: %w", "avata_"+Metrics[i], err)
 		}
